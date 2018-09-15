37c971d0d10875740725b2969ebc704ebc6acf66
Erik <wigo118@hotmail.de>
2018-09-15 15:20:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace idtm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
