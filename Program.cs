627b9822b061fca3125df49c44d9a32caace621d
Erik <wigo118@hotmail.de>
2018-09-15 16:33:43 +0200

    json schema experiment

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
 
-namespace idtm
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+namespace Idtm {
+    class Program {
+        
+
+        static JsonSchema schema = JsonSchema.Parse(File.ReadAllText(Directory.GetCurrentDirectory() + "\\docs\\schema.json"));
+
+        static void Main(string[] args) {
+            string file;
+            if(args.Length == 1 && File.Exists(args[0])){
+                file = args[0];
+            }else if(args.Length != 0){
+                Console.WriteLine("The file you tried to specify doesn't exist");
+            }
+
+            JObject user = JObject.Parse(@"{
+                'IMG_0001.jpg':{'tag1':5, 'tag2':7},
+                'IMG_0002.jpg':{'tag1':4, 'tag2':3},
+                }");
+
+                
+
+            Console.WriteLine(user.IsValid(schema));
+            
+            
+
+
+
+
 
             Console.ReadKey();
         }
