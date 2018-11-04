d955416efee1e46fb587d7316b45a567334ba1b2
Erik <wigo118@hotmail.de>
2018-11-04 01:59:21 +0100

    cleared things up

--- Bio.cs
+++ Bio.cs
@@ -136,74 +136,6 @@ namespace Idtm.IO {
             }
         }
 
-        //REEEMOVE
-/*        public static List<Img> ReadFile(string path){
-            //The Reader
-            JsonTextReader reader = new JsonTextReader(new StreamReader(path));
-            //The List
-            List<Img> imgs = new List<Img>();
-            //Reseting the tagHeader
-            tagHeader.Clear();
-
-            //Just to skip first
-            bool inRoot = false;
-            //A buffer for a var name
-            string name = "";
-            //List counter
-            int imgI = -1;
-            while (reader.Read()){
-                if (reader.Value != null){
-                    Console.WriteLine("{0}, Value: {1}", reader.TokenType, reader.Value);
-                    switch(reader.TokenType.ToString()){
-                        case "PropertyName":
-                            if(name == ""){
-                                //If this is an image name
-                                name = reader.Value.ToString();
-                            }else {
-                                //If this is an Integer name
-                                imgs[imgI].names.Add(reader.Value.ToString());
-                            }
-                            break;
-                        case "Integer":
-                            //Sets value of the Integer
-                            imgs[imgI].values.Add(Int32.Parse(reader.Value.ToString()));
-                            break;
-                        case "String":
-                            if(name == "tag_header"){
-                                //If object/array this string belongs to is named tag_header
-                                tagHeader.Add(reader.Value.ToString());
-                            }
-                            break;
-                    }
-                }else{
-                    Console.WriteLine("Token: {0}", reader.TokenType);
-                    switch(reader.TokenType.ToString()){
-                        case "StartObject":
-                            if(inRoot){
-                                //if we are already in the root object the following object has to be an img
-                                imgs.Add(new Img(name));
-                                imgI++;
-                            }else {
-                                //Now we are in the root
-                                inRoot = true;
-                            }
-                            break;
-                        case "EndObject":
-                            //Reset img name
-                            name = "";
-                            break;
-                        case "EndArray":
-                            //Reset img name
-                            name = "";
-                            break;
-                    }
-                }
-            }
-
-        return imgs;
-
-        }
-        */
 
         //IT WOOORKS ???
         public static bool Validate(string path){
@@ -212,44 +144,6 @@ namespace Idtm.IO {
                 JObject file = JObject.Parse(File.ReadAllText(path));
                 return file.IsValid(schema);
             }
-        }
-
-        //REEEEMOVE
-        public static bool SaveFile(List<Img> imgs, string path){
-            using(StreamWriter sw = new StreamWriter(path))
-            using(JsonWriter jw = new JsonTextWriter(sw)){
-                jw.Formatting = Formatting.Indented;
-
-
-                jw.WriteStartObject();
-
-                foreach(Img img in imgs){
-                    jw.WritePropertyName(img.name);
-                    jw.WriteStartObject();
-                    for(int i = 0; i < img.names.Count; i++){
-                        jw.WritePropertyName(img.names[i]);
-                        jw.WriteValue(img.values[i]);
-                    }
-                    jw.WriteEndObject();
-                }
-                jw.WriteEndObject();
-
-                //Writes the Text
-                sw.WriteLine();
-
-                return true;
-            }
-
-        
-
-        }
-
-        //REEEEMOVE
-        public static bool CreateFile(string path){
-            using(StreamWriter sw = new StreamWriter(path)){
-                sw.WriteLine("{}");
-            }
-            return true;
         }
 
         public static bool RightsExt(string name){
@@ -274,33 +168,4 @@ namespace Idtm.IO {
         
     }
     
-
-
-    //REMOOOVE
-    public class Img {
-        
-        //Stores the values corrosponding to the names
-        public List<int> values = new List<int>();
-        
-        //Stores the names of all the values
-        public List<string> names = new List<string>();
-
-        //stores the name of the image
-        public string name;
-
-        public Img(string name){
-            this.name = name;
-        }
-
-        public static int IndexOf(string name, List<Img> imgs){
-            for(int i = 0; i < imgs.Count; i++){
-                if(imgs[i].name == name){
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-    }
-
 }
